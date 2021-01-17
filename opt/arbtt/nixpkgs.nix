91888f38c2d5d934568b08369a086ffb4de696d1
--global <--global>
2021-01-17 16:20:55 +0100

    arbtt: Use my haskell.nix nixpkgs pin
    This can be found in `toonn/nix-config/haskell.nix/nixpkgs.nix`.

--- opt/arbtt/nixpkgs.nix
+++
@@ -1,9 +0,0 @@
-let
-  haskellNix = import (builtins.fetchTarball
-    ( "https://github.com/input-output-hk/haskell.nix/archive/"
-    + "59cf05606e7efbbc4741ae28fd8cc610cec94eb8.tar.gz"
-    )) {};
-  nixpkgsSrc = haskellNix.sources.nixpkgs-default;
-  nixpkgsArgs = haskellNix.nixpkgsArgs;
-in
-  import nixpkgsSrc nixpkgsArgs
